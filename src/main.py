9e00e0a545de31cb686c9e768f3c51f9dbd096ac
sdk2k01 <sdk2k01@hotmail.com>
2021-07-27 21:27:48 +0530

    sorted permissions, part 5

--- src/main.py
+++ src/main.py
@@ -7,30 +7,42 @@ intents.members=True
 
 client=commands.Bot(command_prefix='-',intents=intents)
 
-missing_perms = ['administrator']
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Invalid Command!")
-    elif isinstance(error, commands.MissingPermissions(missing_perms)):
-        await ctx.send("Must have `administrator` permissions to use this command!")
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
+@load.error
+async def on_load_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Must have `administrator` permissions to use this command!")
+
 @client.command()
 @commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
+
+@unload.error
+async def on_unload_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Must have `administrator` permissions to use this command!")
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
+
+@reload.error
+async def on_reload_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Must have `administrator` permissions to use this command!")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
