ba686e13ff03bf89f31a647a5df9b44b98bd67d6
sdk2k01 <sdk2k01@hotmail.com>
2021-08-15 09:57:58 +0530

    removed space discrepancy

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -1,25 +1,31 @@
+#Imports
 import discord
 import random
 from discord.ext import commands
 
+#Create a class `games` which inherits from the `commands.Cog` class
 class games(commands.Cog):
     def __init__(self, client):
-        self.client=client
+        self.client = client
     
-    @commands.command(aliases = ['cf','tosscoin','tc'])
+    #Command for `coinflip` 
+    @commands.command(aliases = ['cf', 'tosscoin', 'tc'])
     async def coinflip(self, ctx):
-        cf_res = ['Heads','Tails']
+        cf_res = ['Heads', 'Tails']
         res = random.choice(cf_res)
-        embed = discord.Embed(title="Coinflip",description=f'Result = {res}!',color=discord.Color.gold())
-        await ctx.send(embed=embed)
+        embed = discord.Embed(title = "Coinflip", description = f'Result = {res}!', 
+        color = discord.Color.gold())
+        await ctx.send(embed = embed)
 
-    @commands.command(aliases = ['die','6face'])
+    #Command for `die toss`
+    @commands.command(aliases = ['die', '6face'])
     async def tossdie(self, ctx):
         td_res = [int(i) for i in range(1,7)]
         res = random.choice(td_res)
-        embed = discord.Embed(title="Toss A Die",description=f'Result = {res}!',
-        color=discord.Color.magenta())
-        await ctx.send(embed=embed)
+        embed = discord.Embed(title = "Toss A Die", description = f'Result = {res}!',
+        color = discord.Color.magenta())
+        await ctx.send(embed = embed)
 
+#Setup cogs `games`
 def setup(client):
     client.add_cog(games(client))
